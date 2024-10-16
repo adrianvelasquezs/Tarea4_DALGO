1d05523bfe35cefb5a9d1984ecdbf1a87fcac140
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-16 17:53:53 -0500

    Modification

--- problema4/MainProblem4.java
+++ problema4/MainProblem4.java
@@ -16,11 +16,11 @@ public class MainProblem4 {
 
         int w = warehouses.size();
         int l = libraries.size();
-        int n = factories + w + l; // calculate the total number of nodes
+        int n = factories + w + l + 2; // calculate the total number of nodes
         int[][] graph = new int[n][n]; // make adjacency matrix
         buildGraph( graph, trucks, n, factories, w );
 
-
+        totalBooks = edmondsKarp(graph, n - 1); // calculate the maximum flow
 
         return totalBooks;
     }
@@ -36,8 +36,14 @@ public class MainProblem4 {
     private static void buildGraph(int[][] graph, List<Truck> trucks, int n, int f, int w )
     {
         for ( int i = 0; i < n; i++) {
-            Arrays.fill(graph[i], Integer.MAX_VALUE);
+            Arrays.fill(graph[i], Integer.MIN_VALUE);
             graph[i][i] = 0;
+            if ( i < f + 1 ) {
+                graph[0][i] = 0; // connect the source to the factories
+            }
+            if ( i > f + w + 1 ) {
+                graph[i][n - 1] = 0; // connect the warehouses to the sink
+            }
         }
 
         for (Truck truck : trucks) {
@@ -67,7 +73,7 @@ public class MainProblem4 {
      */
     private static int convertToIndex( String s, int f, int w )
     {
-        int index = Integer.parseInt(String.valueOf(s.charAt(1))) - 1;
+        int index = Integer.parseInt(String.valueOf(s.charAt(1)));
         if ( s.charAt(0) == 'F' ) {
             return index;
         }
@@ -78,7 +84,82 @@ public class MainProblem4 {
     }
 
     /**
+     * Implementation of the Edmonds-Karp algorithm to calculate the maximum flow in a graph.
+     *
+     * @param graph the adjacency matrix
+     * @param sink  the sink node
+     * @return the maximum flow
+     */
+    private static int edmondsKarp(int[][] graph, int sink) {
+        int u, v;
+        int n = graph.length;
+        int[][] residualGraph = new int[n][n];
+
+        for (u = 0; u < n; u++) {
+            for (v = 0; v < n; v++) {
+                residualGraph[u][v] = graph[u][v];
+            }
+        }
+
+        int[] parent = new int[n];
+        int maxFlow = 0;
+
+        while ( bfs(residualGraph, sink, parent) ) {
+            int pathFlow = Integer.MAX_VALUE;
+            for ( v = sink; v != 0; v = parent[v] ) {
+                u = parent[v];
+                pathFlow = Math.min(pathFlow, residualGraph[u][v]);
+            }
+
+            for (v = sink; v != 0; v = parent[v]) {
+                u = parent[v];
+                residualGraph[u][v] -= pathFlow;
+                residualGraph[v][u] += pathFlow;
+            }
+
+            maxFlow += pathFlow;
+        }
+
+        return maxFlow;
+    }
+
+    /**
+     * Bfs implementation to search path to sink in residual graph.
+     *
+     * @param residualGraph the residual graph
+     * @param sink          the sink node
+     * @param parent        the parent array
+     * @return true if there is a path to the sink, false otherwise
+     */
+    private static boolean bfs(int[][] residualGraph, int sink, int[] parent) {
+        boolean[] visited = new boolean[residualGraph.length];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited[0] = true;
+        parent[0] = -1;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (int v = 0; v < residualGraph.length; v++) {
+                if (!visited[v] && residualGraph[u][v] > 0) {
+                    if (v == sink) {
+                        parent[v] = u;
+                        return true;
+                    }
+                    queue.add(v);
+                    parent[v] = u;
+                    visited[v] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Main method to test the implementation of the classes.
+     *
      * @param args the arguments
      */
     public static void main(String[] args) {
